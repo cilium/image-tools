70d21deedf61473f760970910e7453aa84e4c67a
Daniel Borkmann <daniel@iogearbox.net>
2020-05-27 22:42:51 +0200

    cilium, llvm: cherry pick MOV_32_64 BPF code-gen optimization
    We have this in our generated BPF code quite a lot, so cherry-pick the
    work that John recently upstreamed into LLVM:
    
      - https://github.com/llvm/llvm-project/commit/29bc5dd19407c4d7cad1c059dea26ee216ddc7ca
      - https://github.com/llvm/llvm-project/commit/13f6c81c5d9a7a34a684363bcaad8eb7c65356fd
    
    Signed-off-by: Daniel Borkmann <daniel@iogearbox.net>

--- images/llvm/checkout-llvm.sh
+++ images/llvm/checkout-llvm.sh
@@ -10,11 +10,13 @@ set -o nounset
 
 rev="llvmorg-10.0.0"
 
-# git clone --branch "${rev}" https://github.com/llvm/llvm-project.git /src/llvm
+git clone --branch "${rev}" https://github.com/llvm/llvm-project.git /src/llvm
+git cherry-pick 29bc5dd19407c4d7cad1c059dea26ee216ddc7ca
+git cherry-pick 13f6c81c5d9a7a34a684363bcaad8eb7c65356fd
 
-curl --fail --show-error --silent --location "https://github.com/llvm/llvm-project/archive/${rev}.tar.gz" --output /tmp/llvm.tgz
-
-mkdir -p /src
-tar -xf /tmp/llvm.tgz -C /tmp
-mv "/tmp/llvm-project-${rev}" /src/llvm
-rm -f /tmp/llvm.tgz
+# curl --fail --show-error --silent --location "https://github.com/llvm/llvm-project/archive/${rev}.tar.gz" --output /tmp/llvm.tgz
+#
+# mkdir -p /src
+# tar -xf /tmp/llvm.tgz -C /tmp
+# mv "/tmp/llvm-project-${rev}" /src/llvm
+# rm -f /tmp/llvm.tgz
