3e62cdd7213b6371698bd5ad19e1d3492e31b6ef
Ilya Dmitrichenko <errordeveloper@gmail.com>
2020-06-08 16:07:34 +0100

    llvm: Fix configuration of the AArch64 build
    BPF bytecode is the target, it is independent of CPU architecture.
    The configuration was originally overlooked in #3.
    
    Fixes: #31
    
    Signed-off-by: Ilya Dmitrichenko <errordeveloper@gmail.com>

--- images/llvm/build-llvm-cross-aarch64.sh
+++ images/llvm/build-llvm-cross-aarch64.sh
@@ -21,12 +21,9 @@ CC="${triplet}-gcc" CXX="${triplet}-g++" \
     -DBUILD_SHARED_LIBS="OFF" \
     -DCMAKE_BUILD_TYPE="Release" \
     -DLLVM_BUILD_RUNTIME="OFF" \
-    -DCMAKE_CROSSCOMPILING="True" \
     -DLLVM_TABLEGEN="/src/llvm/llvm/build-native/bin/llvm-tblgen" \
     -DCLANG_TABLEGEN="/src/llvm/llvm/build-native/bin/clang-tblgen" \
-    -DLLVM_TARGET_ARCH="AArch64" \
-    -DLLVM_TARGETS_TO_BUILD="AArch64" \
-    -DLLVM_DEFAULT_TARGET_TRIPLE="${triplet}"
+    -DCMAKE_CROSSCOMPILING="True"
 
 ninja clang llc
 
