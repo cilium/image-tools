a10c245b77454a02ce73a3f65a8a3bfa76137da6
Ilya Dmitrichenko <errordeveloper@gmail.com>
2020-07-08 10:01:29 +0100

    Refactor Go deps in maker image, add imagine & kg
    - ensure all binaries are static
    - refactor the build to leverage buildkit mounts & cache
    - use latest release of buildx
    
    Signed-off-by: Ilya Dmitrichenko <errordeveloper@gmail.com>

---
+++ images/maker/build-go-deps.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Copyright 2017-2020 Authors of Cilium
+# SPDX-License-Identifier: Apache-2.0
+
+set -o xtrace
+set -o errexit
+set -o pipefail
+set -o nounset
+
+cd /src
+
+unset GOPATH
+
+export CGO_ENABLED=0
+
+mkdir -p /out/usr/local/bin
+
+go build -ldflags '-s -w' -o /out/usr/local/bin/docker-credential-env github.com/errordeveloper/docker-credential-env
+go build -ldflags '-s -w' -o /out/usr/local/bin/imagine github.com/errordeveloper/imagine
+go build -ldflags '-s -w' -o /out/usr/local/bin/kg github.com/errordeveloper/kue/cmd/kg
+go build -ldflags '-s -w' -o /out/usr/local/bin/docker-buildx github.com/docker/buildx/cmd/buildx
