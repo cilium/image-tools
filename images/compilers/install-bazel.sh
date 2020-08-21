e2b83bf2da67b29932fe77057c43a4274b1fb3ff
Michal Rostecki <mrostecki@opensuse.org>
2020-08-21 14:06:43 +0200

    compilers: Add Bazel 3.3.1 and 3.4.1 versions
    Envoy and envoy-build-tools already support and require newer versions
    of Bazel.
    
    Signed-off-by: Michal Rostecki <mrostecki@opensuse.org>

--- images/compilers/install-bazel.sh
+++ images/compilers/install-bazel.sh
@@ -13,6 +13,8 @@ bazel_versions=(
   "2.2.0"
   "3.1.0"
   "3.2.0"
+  "3.3.1"
+  "3.4.1"
 )
 
 # install bazel wrapper script in the path, it automatically recognises `.bazelversion` and `USE_BAZEL_VERSIONS`, if neither are set it picks latest
