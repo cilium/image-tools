6292298fefc9a171f7f51fa5c59a11b8cf2d5ab6
Ilya Dmitrichenko <errordeveloper@gmail.com>
2021-02-02 16:07:05 +0000

    Add a script to lookup commit hash from a tree hash
    Signed-off-by: Ilya Dmitrichenko <errordeveloper@gmail.com>

---
+++ scripts/find-commit-for-tree-hash.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Copyright 2017-2020 Authors of Cilium
+# SPDX-License-Identifier: Apache-2.0
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# This script find the commit hash for a given subdir tree hash.
+# It can be used to find what commit in the subdir was used to build a given image tag.
+
+# TODO: allow different modes, e.g.
+# - show last revision of top-level tree for the given sudir hash, which would encompas
+#   top-level changees since e.g. `.github` and `scripts` etc
+
+if [ "$#" -gt 2 ] ; then
+  echo "$0 supports exactly 2 arguments - tree hash & subdir"
+  exit 1
+fi
+
+tree_hash="${1}"
+dir="${2}"
+
+for i in $(git rev-list @ -- "${dir}") ; do
+  if git ls-tree --full-tree "${i}" -- "${dir}" | grep -q "${tree_hash}" ; then
+    echo "${i}"
+    exit 0
+  fi
+done
+
+exit 1
