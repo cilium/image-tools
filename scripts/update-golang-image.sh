25b009bb44777feaab7d5f712564b54f00d62506
Tobias Klauser <tklauser@distanz.ch>
2021-03-01 12:24:54 +0100

    Bump Go image to 1.16.0
    Signed-off-by: Tobias Klauser <tklauser@distanz.ch>

--- scripts/update-golang-image.sh
+++ scripts/update-golang-image.sh
@@ -19,7 +19,7 @@ root_dir="$(git rev-parse --show-toplevel)"
 
 cd "${root_dir}"
 
-image="${1:-docker.io/library/golang:1.15.7}"
+image="${1:-docker.io/library/golang:1.16.0}"
 
 image_digest="$("${script_dir}/get-image-digest.sh" "${image}")"
 
