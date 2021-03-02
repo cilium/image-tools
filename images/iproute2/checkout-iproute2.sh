ecfb4ee906a41db1518a07be5843c5c56b6a6e10
Paul Chaignon <paul@cilium.io>
2021-03-02 14:58:17 +0100

    iproute2: bump to latest revision of static-data branch
    This version includes a backport of upstream support for setting the
    xfrm output mark's mask [1].
    
    1 - https://github.com/cilium/iproute2/pull/8
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- images/iproute2/checkout-iproute2.sh
+++ images/iproute2/checkout-iproute2.sh
@@ -8,7 +8,7 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-rev="cee19aa079794a7bdb3a64f414e88a6612a5b376"
+rev="474d7b7a102f52d54a9b4a77785104ce7a5c49de"
 
 # git clone https://github.com/cilium/iproute2 /src/iproute2
 # cd /src/iproute2
