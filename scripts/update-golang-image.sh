740992deaf5ef5eb99f409128480befbc68bdfe2
Tobias Klauser <tobias@cilium.io>
2021-04-10 16:04:29 +0200

    Bump Go image to 1.16.3
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

--- scripts/update-golang-image.sh
+++ scripts/update-golang-image.sh
@@ -19,7 +19,7 @@ root_dir="$(git rev-parse --show-toplevel)"
 
 cd "${root_dir}"
 
-image="${1:-docker.io/library/golang:1.16.0}"
+image="${1:-docker.io/library/golang:1.16.3}"
 
 image_digest="$("${script_dir}/get-image-digest.sh" "${image}")"
 
