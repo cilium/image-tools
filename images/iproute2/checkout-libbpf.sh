dcd3c349a8935f8ad501aae00a1062f51c92de7f
Martynas Pumputis <m@lambda.lt>
2021-06-30 16:08:03 +0200

    iproute2: Update iproute2 and libbpf
    libbpf is a fork of github.com/libbpf/libbpf with e979be1529c ("cilium:
    Use old approach for handling .data/.bss") on top of it.
    
    iproute2 is the upstream v5.11 with many patches by us. Once v5.14 has
    been released, we can drop ec3b4a388d4 ("libbpf: fix attach of prog with
    multiple sections"), as it has been upstreamed, but not released yet.
    
    Signed-off-by: Martynas Pumputis <m@lambda.lt>

--- images/iproute2/checkout-libbpf.sh
+++ images/iproute2/checkout-libbpf.sh
@@ -8,14 +8,14 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-rev="e1a90f3768fda85da958c37515f9e8cb6d3d6a92" # master@2021-02-26
+rev="e979be1529ce34209a0db171785645512e56c871"
 
 # git clone https://github.com/libbpf/libbpf /src/libbpf
 # cd /src/libbpf
 # git checkout -b "build-${rev:0:7}" "${rev}
 
 # It is much quicker to download a tarball then a full git checkout,
-curl --fail --show-error --silent --location "https://github.com/libbpf/libbpf/archive/${rev}.tar.gz" --output /tmp/libbpf.tgz
+curl --fail --show-error --silent --location "https://github.com/cilium/libbpf/archive/${rev}.tar.gz" --output /tmp/libbpf.tgz
 mkdir -p /src
 tar -xf /tmp/libbpf.tgz -C /tmp
 mv "/tmp/libbpf-${rev}" /src/libbpf
