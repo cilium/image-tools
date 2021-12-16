7674e60c189211630402b5ce37f5e427e8e20bb2
Tobias Klauser <tobias@cilium.io>
2021-12-16 20:20:58 +0100

    scripts: add script to update Ubuntu image
    The version of the Ubuntu 20.04 image we use hasn't been updated in a
    while and has picked up several vulnerability reports on Quay. Add a
    script and corresponding Makefile target to simplify future updates.
    
    Signed-off-by: Tobias Klauser <tobias@cilium.io>

---
+++ scripts/update-ubuntu-image.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Copyright 2021 Authors of Cilium
+# SPDX-License-Identifier: Apache-2.0
+
+set -o xtrace
+set -o errexit
+set -o pipefail
+set -o nounset
+
+if [ "$#" -gt 1 ] ; then
+  echo "$0 supports at most 1 argument"
+  exit 1
+fi
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+root_dir="$(git rev-parse --show-toplevel)"
+
+cd "${root_dir}"
+
+image="${1:-docker.io/library/ubuntu:20.04}"
+
+image_digest="$("${script_dir}/get-image-digest.sh" "${image}")"
+
+# shellcheck disable=SC2207
+used_by=($(git grep -l UBUNTU_IMAGE= images))
+
+for i in "${used_by[@]}" ; do
+  sed "s|\(UBUNTU_IMAGE=\)docker.io/library/ubuntu:.*\$|\1${image}@${image_digest}|" "${i}" > "${i}.sedtmp" && mv "${i}.sedtmp" "${i}"
+done
