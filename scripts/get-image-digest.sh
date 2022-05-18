9df3edaaa6c567a4c3d7eeb3a2f776c7b30b540b
Tam Mach <tam.mach@cilium.io>
2022-05-18 15:19:36 +1000

    gha: Update tester image
    The changes are updated via make target (e.g. update-tester-image).
    
    Signed-off-by: Tam Mach <tam.mach@cilium.io>

--- scripts/get-image-digest.sh
+++ scripts/get-image-digest.sh
@@ -17,7 +17,8 @@ fi
 root_dir="$(git rev-parse --show-toplevel)"
 
 if [ -z "${MAKER_CONTAINER+x}" ] ; then
-   exec docker run --env DOCKER_HUB_PUBLIC_ACCESS_ONLY=true --env QUAY_PUBLIC_ACCESS_ONLY=true --rm --volume "${root_dir}:/src" --workdir /src "${MAKER_IMAGE}" "/src/scripts/$(basename "${0}")" "${1}"
+   exec docker run --env DOCKER_HUB_PUBLIC_ACCESS_ONLY=true --env QUAY_PUBLIC_ACCESS_ONLY=true --rm --volume "${root_dir}:/src" --workdir /src "${MAKER_IMAGE}" \
+    sh -c "git config --global --add safe.directory /src && /src/scripts/$(basename "${0}") \"${1}\""
 fi
 
 crane digest "${1}" 2> /dev/null
