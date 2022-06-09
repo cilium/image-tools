948bc8228d7e894007854a8387fec66512e0fdbd
Paul Chaignon <paul@cilium.io>
2022-06-09 22:32:24 +0200

    llvm: Bump LLVM to 14.0.4
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- images/llvm/checkout-llvm.sh
+++ images/llvm/checkout-llvm.sh
@@ -8,17 +8,12 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-rev="llvmorg-10.0.0"
+rev="llvmorg-14.0.4"
 
 git config --global user.email "maintainer@cilium.io"
 git config --global user.name  "Cilium Maintainers"
 
 git clone --branch "${rev}" https://github.com/llvm/llvm-project.git /src/llvm
-
-cd /src/llvm
-git cherry-pick 29bc5dd19407c4d7cad1c059dea26ee216ddc7ca
-git cherry-pick 13f6c81c5d9a7a34a684363bcaad8eb7c65356fd
-cd -
 
 # curl --fail --show-error --silent --location "https://github.com/llvm/llvm-project/archive/${rev}.tar.gz" --output /tmp/llvm.tgz
 #
