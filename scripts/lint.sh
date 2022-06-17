c1a743ec280289353ed85ee0a5a16ae3bd41f25f
Paul Chaignon <paul@cilium.io>
2022-06-18 00:29:26 +0200

    Revert "gha: Update image-maker tag"
    This reverts commit 8ffae82a270aed2baf7da687f9566705ebf98439.
    
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -7,13 +7,12 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
-MAKER_IMAGE="${MAKER_IMAGE:-quay.io/cilium/image-maker:53e0a6dcaf1d282c890e220ad069bbaa6cd9c72c}"
+MAKER_IMAGE="${MAKER_IMAGE:-quay.io/cilium/image-maker:9e2e7ad1a524cf714d491945e90fe650125cd60a}"
 
 root_dir="$(git rev-parse --show-toplevel)"
 
 if [ -z "${MAKER_CONTAINER+x}" ] ; then
-   exec docker run --rm --volume "${root_dir}:/src" --workdir /src "${MAKER_IMAGE}" \
-    sh -c "git config --global --add safe.directory /src && /src/scripts/$(basename "${0}")"
+   exec docker run --rm --volume "${root_dir}:/src" --workdir /src "${MAKER_IMAGE}" "/src/scripts/$(basename "${0}")"
 fi
 
 cd "${root_dir}"
