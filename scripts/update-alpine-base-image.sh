2fa25b585a9e4bc6b5eb88ced256283c99e4b8b0
Michi Mutsuzaki <michi@isovalent.com>
2023-05-31 00:11:16 +0000

    Pick up Alpine 3.18.0
    Update Alpine by bumping up the version in update-alpine-base-image.sh
    and running `make update-alpine-base-image`.
    
    Ref: https://hub.docker.com/_/alpine/tags
    
    Signed-off-by: Michi Mutsuzaki <michi@isovalent.com>

--- scripts/update-alpine-base-image.sh
+++ scripts/update-alpine-base-image.sh
@@ -19,7 +19,7 @@ root_dir="$(git rev-parse --show-toplevel)"
 
 cd "${root_dir}"
 
-image="${1:-docker.io/library/alpine:3.16.0}"
+image="${1:-docker.io/library/alpine:3.18.0}"
 
 image_digest="$("${script_dir}/get-image-digest.sh" "${image}")"
 
