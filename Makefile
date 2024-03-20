cilium/image-tools

commit 0ee97a0754ad7a29f514ca0b43663ea8d7dcf187

Allow users to overwrite build platforms
This commit modifies the Makefile to make platforms stored in a variable,
which can be adjusted by users as needed. For instance, if a user is
only interested in amd64 builds, they can use:

`PLATFORMS=linux/amd64 make all-images`

Signed-off-by: Ryan Drew <ryan.drew@isovalent.com>

Makefile
@@ -5,6 +5,7 @@ REGISTRIES ?= quay.io/cilium
 
 PUSH ?= false
 EXPORT ?= false
+PLATFORMs ?= linux/amd64,linux/arm64
 
 all-images: lint maker-image update-maker-image tester-image update-tester-image compilers-image update-compilers-image bpftool-image llvm-image network-perf-image
 
@@ -12,7 +13,7 @@ lint:
 	scripts/lint.sh
 
 .buildx_builder:
-	docker buildx create --platform linux/amd64,linux/arm64 --buildkitd-flags '--debug' > $@
+	docker buildx create --platform $(PLATFORMS) --buildkitd-flags '--debug' > $@
 
 update-alpine-base-image:
 	scripts/update-alpine-base-image.sh
@@ -24,43 +25,43 @@ update-ubuntu-image:
 	scripts/update-ubuntu-image.sh
 
 maker-image: .buildx_builder
-	PUSH=$(PUSH) EXPORT=$(EXPORT) scripts/build-image.sh image-maker images/maker linux/amd64,linux/arm64 "$$(cat .buildx_builder)" $(REGISTRIES)
+	PUSH=$(PUSH) EXPORT=$(EXPORT) scripts/build-image.sh image-maker images/maker $(PLATFORMS) "$$(cat .buildx_builder)" $(REGISTRIES)
 
 update-maker-image:
 	scripts/update-maker-image.sh $(firstword $(REGISTRIES))
 
 tester-image: .buildx_builder
-	PUSH=$(PUSH) EXPORT=$(EXPORT) TEST=true scripts/build-image.sh image-tester images/tester linux/amd64,linux/arm64 "$$(cat .buildx_builder)" $(REGISTRIES)
+	PUSH=$(PUSH) EXPORT=$(EXPORT) TEST=true scripts/build-image.sh image-tester images/tester $(PLATFORMS) "$$(cat .buildx_builder)" $(REGISTRIES)
 
 update-tester-image:
 	scripts/update-tester-image.sh $(firstword $(REGISTRIES))
 
 compilers-image: .buildx_builder
-	PUSH=$(PUSH) EXPORT=$(EXPORT) TEST=true scripts/build-image.sh image-compilers images/compilers linux/amd64,linux/arm64 "$$(cat .buildx_builder)" $(REGISTRIES)
+	PUSH=$(PUSH) EXPORT=$(EXPORT) TEST=true scripts/build-image.sh image-compilers images/compilers $(PLATFORMS) "$$(cat .buildx_builder)" $(REGISTRIES)
 
 update-compilers-image:
 	scripts/update-compilers-image.sh $(firstword $(REGISTRIES))
 
 bpftool-image: .buildx_builder
-	PUSH=$(PUSH) EXPORT=$(EXPORT) TEST=true scripts/build-image.sh cilium-bpftool images/bpftool linux/amd64,linux/arm64 "$$(cat .buildx_builder)" $(REGISTRIES)
+	PUSH=$(PUSH) EXPORT=$(EXPORT) TEST=true scripts/build-image.sh cilium-bpftool images/bpftool $(PLATFORMS) "$$(cat .buildx_builder)" $(REGISTRIES)
 
 llvm-image: .buildx_builder
-	PUSH=$(PUSH) EXPORT=$(EXPORT) TEST=true scripts/build-image.sh cilium-llvm images/llvm linux/amd64,linux/arm64 "$$(cat .buildx_builder)" $(REGISTRIES)
+	PUSH=$(PUSH) EXPORT=$(EXPORT) TEST=true scripts/build-image.sh cilium-llvm images/llvm $(PLATFORMS) "$$(cat .buildx_builder)" $(REGISTRIES)
 
 ca-certificates-image: .buildx_builder
-	PUSH=$(PUSH) EXPORT=$(EXPORT) scripts/build-image.sh ca-certificates images/ca-certificates linux/amd64,linux/arm64 "$$(cat .buildx_builder)" $(REGISTRIES)
+	PUSH=$(PUSH) EXPORT=$(EXPORT) scripts/build-image.sh ca-certificates images/ca-certificates $(PLATFORMS) "$$(cat .buildx_builder)" $(REGISTRIES)
 
 startup-script-image: .buildx_builder
-	PUSH=$(PUSH) EXPORT=$(EXPORT) scripts/build-image.sh startup-script images/startup-script linux/amd64,linux/arm64 "$$(cat .buildx_builder)" $(REGISTRIES)
+	PUSH=$(PUSH) EXPORT=$(EXPORT) scripts/build-image.sh startup-script images/startup-script $(PLATFORMS) "$$(cat .buildx_builder)" $(REGISTRIES)
 
 checkpatch-image: .buildx_builder
-	PUSH=$(PUSH) EXPORT=$(EXPORT) scripts/build-image.sh cilium-checkpatch images/checkpatch linux/amd64,linux/arm64 "$$(cat .buildx_builder)" $(REGISTRIES)
+	PUSH=$(PUSH) EXPORT=$(EXPORT) scripts/build-image.sh cilium-checkpatch images/checkpatch $(PLATFORMS) "$$(cat .buildx_builder)" $(REGISTRIES)
 
 network-perf-image: .buildx_builder
-	PUSH=$(PUSH) EXPORT=$(EXPORT) scripts/build-image.sh network-perf images/network-perf linux/amd64,linux/arm64 "$$(cat .buildx_builder)" $(REGISTRIES)
+	PUSH=$(PUSH) EXPORT=$(EXPORT) scripts/build-image.sh network-perf images/network-perf $(PLATFORMS) "$$(cat .buildx_builder)" $(REGISTRIES)
 
 iptables-image: .buildx_builder
-	PUSH=$(PUSH) EXPORT=$(EXPORT) scripts/build-image.sh iptables images/iptables linux/amd64,linux/arm64 "$$(cat .buildx_builder)" $(REGISTRIES)
+	PUSH=$(PUSH) EXPORT=$(EXPORT) scripts/build-image.sh iptables images/iptables $(PLATFORMS) "$$(cat .buildx_builder)" $(REGISTRIES)
 
 iptables-20.04-image: .buildx_builder
-	PUSH=$(PUSH) EXPORT=$(EXPORT) scripts/build-image.sh iptables-20.04 images/iptables-20.04 linux/amd64,linux/arm64 "$$(cat .buildx_builder)" $(REGISTRIES)
+	PUSH=$(PUSH) EXPORT=$(EXPORT) scripts/build-image.sh iptables-20.04 images/iptables-20.04 $(PLATFORMS) "$$(cat .buildx_builder)" $(REGISTRIES)

commit a778816813bfda0124cc8b83637c6fc182b23a2c

Makefile: fix definition of PLATFORMS variable
Otherwise the build will fail if PLATFORMS is not defined on make
invocation.

Fixes: 0ee97a0754ad ("Allow users to overwrite build platforms")

Signed-off-by: Tobias Klauser <tobias@cilium.io>

Makefile
@@ -5,7 +5,7 @@ REGISTRIES ?= quay.io/cilium
 
 PUSH ?= false
 EXPORT ?= false
-PLATFORMs ?= linux/amd64,linux/arm64
+PLATFORMS ?= linux/amd64,linux/arm64
 
 all-images: lint maker-image update-maker-image tester-image update-tester-image compilers-image update-compilers-image bpftool-image llvm-image network-perf-image
 