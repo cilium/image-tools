96081ab6a4ef20ea43b5c89ff2241fda5191df9d
Dylan Reimerink <dylan.reimerink@isovalent.com>
2024-11-12 15:50:02 +0100

    images: Add clang-format image
    This commit adds a clang-format image that can be used to format C code.
    The image is made to be as minimal as possible, just a statically
    compiled clang-format binary in a scratch container. This makes it
    quick to pull and allows for the extraction of the binary to run on
    a host system.
    
    Signed-off-by: Dylan Reimerink <dylan.reimerink@isovalent.com>

---
+++ images/clang-format/checkout-llvm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Copyright 2017-2024 Authors of Cilium
+# SPDX-License-Identifier: Apache-2.0
+
+set -o xtrace
+set -o errexit
+set -o pipefail
+set -o nounset
+
+rev="llvmorg-17.0.6"
+
+git config --global user.email "maintainer@cilium.io"
+git config --global user.name  "Cilium Maintainers"
+
+git clone --branch "${rev}" https://github.com/llvm/llvm-project.git /src/llvm
