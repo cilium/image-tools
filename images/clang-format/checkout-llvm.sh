a6abbc8d9af1aa6e835f3b6b831cccb539da425f
Hadrien Patte <hadrien.patte@datadoghq.com>
2025-06-23 23:01:57 +0200

    clang-format: Only compile for the relevant platform
    Signed-off-by: Hadrien Patte <hadrien.patte@datadoghq.com>

--- images/clang-format/checkout-llvm.sh
+++ images/clang-format/checkout-llvm.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Copyright 2017-2024 Authors of Cilium
+# Copyright 2017-2025 Authors of Cilium
 # SPDX-License-Identifier: Apache-2.0
 
 set -o xtrace
@@ -11,6 +11,6 @@ set -o nounset
 rev="llvmorg-17.0.6"
 
 git config --global user.email "maintainer@cilium.io"
-git config --global user.name  "Cilium Maintainers"
+git config --global user.name "Cilium Maintainers"
 
 git clone --branch "${rev}" https://github.com/llvm/llvm-project.git /src/llvm
