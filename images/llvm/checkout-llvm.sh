9daceb2254bc24e8620b1f2bce03e94b38f22fae
Hadrien Patte <hadrien.patte@datadoghq.com>
2025-06-24 14:02:17 +0200

    llvm: Only compile for the relevant platform
    Signed-off-by: Hadrien Patte <hadrien.patte@datadoghq.com>

--- images/llvm/checkout-llvm.sh
+++ images/llvm/checkout-llvm.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Copyright Authors of Cilium
 # SPDX-License-Identifier: Apache-2.0
@@ -11,7 +11,7 @@ set -o nounset
 rev="llvmorg-19.1.7"
 
 git config --global user.email "maintainer@cilium.io"
-git config --global user.name  "Cilium Maintainers"
+git config --global user.name "Cilium Maintainers"
 
 git clone --branch "${rev}" https://github.com/llvm/llvm-project.git /src/llvm
 
