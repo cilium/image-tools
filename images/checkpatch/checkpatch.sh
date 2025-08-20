cilium/image-tools

commit 5d11067d6e0706f327770d52411d00a0965b2486

checkpatch: make script exit if command exits with error
Make the checkpatch wrapper script exit with a non-zero error code if
any command fails, or it attempts to access undefined variables, to
prevent silent failures.

Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

images/checkpatch/checkpatch.sh
@@ -2,6 +2,8 @@
 # SPDX-License-Identifier: Apache-2.0
 # Copyright Authors of Cilium
 
+set -eu -o pipefail
+
 # Default options for checkpatch
 options=(
     --no-tree

commit 62bf9befaeaa8c472c0a943f5e16cd6d4fd65a2e

checkpatch: prevent unbound variable error
The blamed commit set the option to make the script fail if it tries to
access any unbound variables, to catch possible errors. However, the
script now fails when invoked directly, due to:

> ./checkpatch.sh: line 218: GITHUB_REF: unbound variable

While this is not a problem in practice, given that the Cilium's
Makefile always sets the GITHUB_REF variable (possibly empty), let's
guard the variable accesses, to be a bit more user-friendly.

Fixes: 5d11067d6e07 ("checkpatch: make script exit if command exits with error")
Signed-off-by: Marco Iorio <marco.iorio@isovalent.com>

images/checkpatch/checkpatch.sh
@@ -215,7 +215,7 @@ fi
 
 check_cmd git ifne jq
 
-if [ -n "$GITHUB_REF" ]; then
+if [ -n "${GITHUB_REF-}" ]; then
     # Running as GitHub action
     # We'll run checkpatch on each commit from the PR
     check_cmd curl
@@ -256,11 +256,11 @@ ret=0
 for ((i=0; i<nb_commits; i++)); do
     sha=$(echo "$list_commits" | jq -r ".[$i].sha")
     subject=$(echo "$list_commits" | jq -r ".[$i].subject")
-    check_commit "$((i+1))" "$nb_commits" "$sha" "$subject" "$GITHUB_REF"
+    check_commit "$((i+1))" "$nb_commits" "$sha" "$subject" "${GITHUB_REF-}"
 done
 
 # If not a GitHub action and repo is dirty, run on diff from HEAD
-if [ -z "$GITHUB_REF" ] && ! (git diff --exit-code && git diff --cached --exit-code) >/dev/null; then
+if [ -z "${GITHUB_REF-}" ] && ! (git diff --exit-code && git diff --cached --exit-code) >/dev/null; then
     echo "========================================================="
     echo -e "${HL_START}Running on changes from local HEAD$HL_END"
     echo "========================================================="